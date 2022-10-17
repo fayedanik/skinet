6fecc8d32e0e4b25e5e57ca9df75252c8d7267b8
fayedanik <fayedbinshowkatanik@gmail.com>
2022-10-17 19:09:19 +0600

    Product dbcontext addedd

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,17 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer("");
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
